5bde4bce0799e321732ed7f2c684c6650e867a50
IgorLopesOliveira <igor.lopes.oliveira@student.ehb.be>
2025-05-26 15:51:58 +0200

    Refactor Velostat data display to show separate values for right and left sensors

--- public/script.js
+++ public/script.js
@@ -1,29 +1,29 @@
-const socket = io();
+// const socket = io();
 
-const punchDisplay = document.getElementById("punch");
-const countDisplay = document.getElementById("count");
-const axDisplay = document.getElementById("ax");
-const ayDisplay = document.getElementById("ay");
-const azDisplay = document.getElementById("az");
+// const punchDisplay = document.getElementById("punch");
+// const countDisplay = document.getElementById("count");
+// const axDisplay = document.getElementById("ax");
+// const ayDisplay = document.getElementById("ay");
+// const azDisplay = document.getElementById("az");
 
 
-let totalPunches = 0;
-let lastPunch = "";
-let lastTime = 0;
+// let totalPunches = 0;
+// let lastPunch = "";
+// let lastTime = 0;
 
-socket.on("punch", (data) => {
-  if (!data || !data.type) return;
+// socket.on("punch", (data) => {
+//   if (!data || !data.type) return;
 
-  punchDisplay.textContent = data.type;
-  axDisplay.textContent = "X-Axis: " + data.ax;
-  ayDisplay.textContent = "Y-Axis: " + data.ay;
-  azDisplay.textContent = "Z-Axis: " + data.az;
+//   punchDisplay.textContent = data.type;
+//   axDisplay.textContent = "X-Axis: " + data.ax;
+//   ayDisplay.textContent = "Y-Axis: " + data.ay;
+//   azDisplay.textContent = "Z-Axis: " + data.az;
 
-  const now = Date.now();
-  if (data.type !== "Still" && (data.type !== lastPunch || now - lastTime > 500)) {
-    totalPunches++;
-    countDisplay.textContent = "Total Punches: " + totalPunches;
-    lastPunch = data.type;
-    lastTime = now;
-  }
-});
+//   const now = Date.now();
+//   if (data.type !== "Still" && (data.type !== lastPunch || now - lastTime > 500)) {
+//     totalPunches++;
+//     countDisplay.textContent = "Total Punches: " + totalPunches;
+//     lastPunch = data.type;
+//     lastTime = now;
+//   }
+// });
